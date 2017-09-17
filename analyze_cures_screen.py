8724ca108e1caa91ce26bcbf0722073ca05bf1dd
Peter Komar <peter.komar.hu@gmail.com>
2017-09-16 20:13:40 -0400

    OCR for cure names is implemented

--- analyze_cures_screen.py
+++ analyze_cures_screen.py
@@ -1,7 +1,14 @@
 import numpy as np
 import basic_image_operations as bio
 
-def cut_boxes(im_bw):
+from skimage import io
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+import cv2
+from difflib import SequenceMatcher
+
+def cut_boxes(im):
     """
     Locates effect boxes and process boxes on the b&w
     screen shot of the Cures screen.
@@ -9,6 +16,7 @@ def cut_boxes(im_bw):
     # find triangles
     kernel = np.load('./kernels/triangle.npy')
     th = 950
+    im_bw = bio.make_bw(im)
     triangle_centers = bio.find_shapes(im_bw, kernel, th)
     triangle_centers.sort()
 
@@ -74,7 +82,7 @@ def cut_boxes(im_bw):
         elif box_identities[0] == 'process':
             box_height = process_box_height
         x, y = col[0]
-        y1 = y - padding - drug_box_height
+        y1 = y - padding - box_height
         y2 = y - padding
         x1 = x - box_width // 2
         x2 = x + box_width // 2
@@ -87,7 +95,7 @@ def cut_boxes(im_bw):
             box_height = process_box_height
         x, y = col[-1]
         y1 = y + last_padding
-        y2 = y + last_padding + drug_box_height
+        y2 = y + last_padding + box_height
         x1 = x - box_width // 2
         x2 = x + box_width // 2
         box_coordinates.append((x1, x2, y1, y2))
@@ -99,6 +107,26 @@ def cut_boxes(im_bw):
         columns_of_boxes.append(boxes)
         for box_coord in box_coordinates:
             x1, x2, y1, y2 = box_coord
-            boxes.append(im_bw[int(y1):int(y2), int(x1):int(x2)])
+            boxes.append(im[int(y1):int(y2), int(x1):int(x2), :])
 
     return columns_of_boxes, columns_of_box_identities
+
+
+def read_cure_name(im_segment, known_cures, tmp_dir='./tmp/'):
+    tmp_path = tmp_dir + 'tmp.png'
+    tmp_gray_path = tmp_dir + 'tmp-gray.png'
+    io.imsave(tmp_path, im_segment)
+    colored = cv2.imread(tmp_path)
+    gray = cv2.cvtColor(colored, cv2.COLOR_BGR2GRAY)
+    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    cv2.imwrite(tmp_gray_path, gray)
+
+    raw_text = pytesseract.image_to_string(Image.open(tmp_gray_path))
+
+    matches = []
+    for cure in known_cures:
+        match_score = SequenceMatcher(None, raw_text, cure).ratio()
+        matches.append((match_score, cure))
+    best_score, best_text = max(matches, key=lambda t: t[0])
+    return raw_text, best_text, best_score
