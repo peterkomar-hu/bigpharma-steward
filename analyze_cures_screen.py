cdcba0b40d449b231f51e5c8db8b0623d9c5f426
Peter Komar <peter.komar.hu@gmail.com>
2017-09-24 18:42:25 -0400

    created CureBoxReader class

--- analyze_cures_screen.py
+++ analyze_cures_screen.py
@@ -133,48 +133,6 @@ def cut_boxes(im):
     return columns_of_boxes, columns_of_box_identities
 
 
-def read_cure_name(cure_box, known_cures, tmp_dir='./tmp/', th=167):
-    """
-    Recognize the name of the cure, with the aid of a
-    list of known cure names
-    """
-    tmp_path = tmp_dir + 'tmp.png'
-    tmp_gray_path = tmp_dir + 'tmp-gray.png'
-    im_segment = cure_box[:, :200, :]
-    io.imsave(tmp_path, im_segment)
-    colored = cv2.imread(tmp_path)
-    gray = cv2.cvtColor(colored, cv2.COLOR_BGR2GRAY)
-    gray = cv2.threshold(gray, th, 255, cv2.THRESH_BINARY)[1]
-    cv2.imwrite(tmp_gray_path, gray)
-
-    raw_text = pytesseract.image_to_string(Image.open(tmp_gray_path))
-
-    matches = []
-    for cure in known_cures:
-        match_score = SequenceMatcher(None, raw_text, cure).ratio()
-        matches.append((match_score, cure))
-    best_score, best_text = max(matches, key=lambda t: t[0])
-    return raw_text, best_text, best_score
-
-
-def cut_price(box):
-    """
-    Selects the part of the image that contains
-    the price of the cure after the dollar sign
-    """
-    kernel = np.load('./kernels/dollar.npy')
-    th = 650
-    bw = bio.make_bw(box)
-
-    dollar_centers = bio.find_shapes(bw, kernel, th)
-    if len(dollar_centers) != 1:
-        return None
-    x, y = dollar_centers[0]
-    height = 20
-    padding = 10
-    return box[int(y) - height // 2:int(y) + height // 2, int(x) + padding:]
-
-
 def read_integer(im_segment, tmp_dir='./tmp/', th=200):
     """
     Recognize a single integer in a segmented image.
@@ -231,116 +189,194 @@ def read_integer_range(im_segment, tmp_dir='./tmp/', th=170):
     return numbers
 
 
-def read_cure_price(cure_box, tmp_dir='./tmp/', th=200):
-    price_box = cut_price(cure_box)
-    return read_integer(price_box, tmp_dir=tmp_dir, th=th)
+def read_digits_by_kernels(segment, list_of_digit_kernels):
+    im_bw = bio.make_bw(segment, th=170)
+    digits = []
+    for digit in range(0, 10, 1):
+        kernel = list_of_digit_kernels[digit]
+        positions = bio.find_shapes(im_bw, kernel, th=160)
+        for pos in positions:
+            x = pos[0]
+            digits.append((x, digit))
+    digits.sort()
+    return tuple([d[1] for d in digits])
 
 
-def read_cure_slider(slider):
-    """
-    Determines min, max and optimal effective concentration,
-    using the colors of the teeth of the slider.
-    """
-    palette = {
-        'gray': [139, 148, 151],
-        'yellow': [250, 255, 156],
-        'lighter_blue': [99, 255, 252],
-        'green': [101, 234, 108],
-        'blue': [102, 236, 231],
-        'purple': [214, 166, 255],
-        'orange': [254, 167, 89]
-    }
-    colors = []
-    for idx in range(0, 20):
-        x = idx * 12
-        teeth = slider[2:14, x + 2:x + 9]
-        color = np.mean(np.mean(teeth, axis=0), axis=0)
-        color_name = bio.recognize_color(color, palette)
-        colors.append(color_name)
-    if 'green' in colors:
-        del (palette['blue'])
-    else:
-        del (palette['lighter_blue'])
-    colors = []
-    for idx in range(0, 20, 1):
-        x = idx * 12
-        teeth = slider[2:14, x + 2:x + 9]
-        color = np.mean(np.mean(teeth, axis=0), axis=0)
-        color_name = bio.recognize_color(color, palette)
-        colors.append(color_name)
+class CureBoxReader:
+    def __init__(self):
+        self.dollar_kernel = np.load('./kernels/dollar.npy')
+        self.active_range_text_kernel = np.load('./kernels/Active-Range-text.npy')
+        self.max_text_kernel = np.load('./kernels/Max-text.npy')
+        self.flask_kernel = np.load('./kernels/flask.npy')
+        with open('./kernels/list-of-cures.txt', 'r') as f_cures:
+            self.known_cures = f_cures.read().split('\n')
+        self.conc_digits = []
+        for digit in range(0, 10, 1):
+            kernel = np.load('./kernels/conc_' + str(digit) + '_kernel.npy')
+            self.conc_digits.append(kernel)
 
-    effect_colors = ['green', 'blue', 'purple', 'orange', 'yellow']
-    conc_low = None
-    conc_optimal = None
-    conc_high = None
-    for idx, cname in enumerate(colors):
-        conc = idx + 1
-        if cname in effect_colors:
-            conc_high = conc
-            if conc_low is None:
-                conc_low = conc
-        if cname == 'yellow':
-            conc_optimal = conc
+    def _cut_price(self, cure_box):
+        """
+        Selects the part of the image that contains
+        the price of the cure after the dollar sign
+        """
+        bw = bio.make_bw(cure_box)
+        dollar_centers = bio.find_shapes(bw, self.dollar_kernel, th=650)
+        if len(dollar_centers) != 1:
+            return None
+        x, y = dollar_centers[0]
+        height = 20
+        padding = 10
+        return cure_box[int(y) - height // 2:int(y) + height // 2, int(x) + padding:]
 
-    return [conc_low, conc_high], conc_optimal
+    def _read_price(self, cure_box, tmp_dir='./tmp/', th=200):
+        price_box = self._cut_price(cure_box)
+        return read_integer(price_box, tmp_dir=tmp_dir, th=th)
 
+    def _read_cure_name(self, cure_box, tmp_dir='./tmp/', th=167):
+        """
+        Recognize the name of the cure, with the aid of a
+        list of known cure names
+        """
+        tmp_path = tmp_dir + 'tmp.png'
+        tmp_gray_path = tmp_dir + 'tmp-gray.png'
+        im_segment = cure_box[:, :200, :]
+        io.imsave(tmp_path, im_segment)
+        colored = cv2.imread(tmp_path)
+        gray = cv2.cvtColor(colored, cv2.COLOR_BGR2GRAY)
+        gray = cv2.threshold(gray, th, 255, cv2.THRESH_BINARY)[1]
+        cv2.imwrite(tmp_gray_path, gray)
 
-def read_hoover_cure_concentrations(cure_box):
-    box_bw = bio.make_bw(cure_box)
+        raw_text = pytesseract.image_to_string(Image.open(tmp_gray_path))
 
-    active_range_text_kernel = np.load('./kernels/Active-Range-text.npy')
-    active_range_text_position = bio.find_shapes(box_bw, active_range_text_kernel, 1850)
-    assert len(active_range_text_position) == 1
+        matches = []
+        for cure in self.known_cures:
+            match_score = SequenceMatcher(None, raw_text, cure).ratio()
+            matches.append((match_score, cure))
+        best_score, best_text = max(matches, key=lambda t: t[0])
+        return raw_text, best_text, best_score
 
-    max_text_kernel = np.load('./kernels/Max-text.npy')
-    max_text_position = bio.find_shapes(box_bw, max_text_kernel, 700)
-    assert len(max_text_position) == 1
+    def _read_cure_slider(self, slider):
+        """
+        Determines min, max and optimal effective concentration,
+        using the colors of the teeth of the slider.
+        """
+        palette = {
+            'gray': [139, 148, 151],
+            'yellow': [250, 255, 156],
+            'lighter_blue': [99, 255, 252],
+            'green': [101, 234, 108],
+            'blue': [102, 236, 231],
+            'purple': [214, 166, 255],
+            'orange': [254, 167, 89]
+        }
+        colors = []
+        for idx in range(0, 20):
+            x = idx * 12
+            teeth = slider[2:14, x + 2:x + 9]
+            color = np.mean(np.mean(teeth, axis=0), axis=0)
+            color_name = bio.recognize_color(color, palette)
+            colors.append(color_name)
+        if 'green' in colors:
+            del (palette['blue'])
+        else:
+            del (palette['lighter_blue'])
+        colors = []
+        for idx in range(0, 20, 1):
+            x = idx * 12
+            teeth = slider[2:14, x + 2:x + 9]
+            color = np.mean(np.mean(teeth, axis=0), axis=0)
+            color_name = bio.recognize_color(color, palette)
+            colors.append(color_name)
 
-    flask_kernel = np.load('./kernels/flask.npy')
-    flask_positions = bio.find_shapes(box_bw, flask_kernel, 500)
-    assert len(flask_positions) == 2
-
-    arx, ary = active_range_text_position[0]
-    mx, my = max_text_position[0]
-    f1x, f1y = flask_positions[0]
-    f2x, f2y = flask_positions[1]
-
-    range_coordinates = [[arx + 50, f1x - 12], [ary - 10, ary + 10]]
-    max_coordinates = [[mx + 20, f2x - 12], [my - 10, my + 10]]
-    slider_coordinates = [[arx - 51, arx + 189], [ary + 25, ary + 42]]
-
-    conc_range = read_integer_range(bio.cut(cure_box, range_coordinates))
-    conc_optimal = read_integer(bio.cut(cure_box, max_coordinates))
-
-    conc_range_slider, conc_optimal_slider = read_cure_slider(bio.cut(cure_box, slider_coordinates))
-
-    if conc_range[0] != conc_range_slider[0]:
-        conc_range[0] = None
-    if conc_range[1] != conc_range_slider[1]:
-        conc_range[1] = None
-    if conc_optimal != conc_optimal_slider:
+        effect_colors = ['green', 'blue', 'purple', 'orange', 'yellow']
+        conc_low = None
         conc_optimal = None
+        conc_high = None
+        for idx, cname in enumerate(colors):
+            conc = idx + 1
+            if cname in effect_colors:
+                conc_high = conc
+                if conc_low is None:
+                    conc_low = conc
+            if cname == 'yellow':
+                conc_optimal = conc
 
-    return conc_range, conc_optimal
+        return [conc_low, conc_high], conc_optimal
 
+    # def _read_concentration(self, segment):
+    #     digits = read_digits_by_kernels(segment, self.conc_digits)
+    #     if len(digits) < 1:
+    #         return None
+    #     return int(''.join([str(d) for d in digits]))
+    #
+    # def _read_concentration_range(self, segment):
+    #     digits = read_digits_by_kernels(segment, self.conc_digits)
+    #     if len(digits) == 2:
+    #         low = digits[0]
+    #         high = digits[1]
+    #     elif len(digits) == 3:
+    #         low = digits[0]
+    #         high = 10*digits[1] + digits[2]
+    #     elif len(digits) == 4:
+    #         low = 10*digits[0] + digits[1]
+    #         high = 10*digits[2] + digits[3]
+    #     else:
+    #         return [None, None]
+    #     return [low, high]
 
-def analyze_cure_box(cure_box, known_cures):
-    """
-    Determines if the cure box is hoovered over or not,
-    and recognizes the information contained within.
-    """
-    info = {}
-    price_box = cut_price(cure_box)
-    if price_box is not None:
-        info['price'] = read_integer(price_box)
-        _, name, _ = read_cure_name(cure_box, known_cures)
-        info['name'] = name
-    else:
-        conc_range, conc_optimal = read_hoover_cure_concentrations(cure_box)
-        info['conc_range'] = tuple(conc_range)
-        info['conc_optimal'] = conc_optimal
+    def _read_hoover_cure_concentrations(self, cure_box):
+        box_bw = bio.make_bw(cure_box)
 
-    return info
+        active_range_text_position = bio.find_shapes(box_bw, self.active_range_text_kernel, 1850)
+        assert len(active_range_text_position) == 1
+
+        # max_text_position = bio.find_shapes(box_bw, self.max_text_kernel, 700)
+        # assert len(max_text_position) == 1
+
+        # flask_positions = bio.find_shapes(box_bw, self.flask_kernel, 500)
+        # assert len(flask_positions) == 2
+
+        arx, ary = active_range_text_position[0]
+        # mx, my = max_text_position[0]
+        # f1x, f1y = flask_positions[0]
+        # f2x, f2y = flask_positions[1]
+
+        # range_coordinates = [[arx + 50, f1x - 12], [ary - 10, ary + 10]]
+        # max_coordinates = [[mx + 20, f2x - 12], [my - 10, my + 10]]
+        slider_coordinates = [[arx - 51, arx + 189], [ary + 25, ary + 42]]
+
+        # conc_optimal = self._read_concentration(bio.cut(cure_box, max_coordinates))
+        # conc_range = self._read_concentration_range(bio.cut(cure_box, range_coordinates))
+
+        conc_range_slider, conc_optimal_slider = self._read_cure_slider(bio.cut(cure_box, slider_coordinates))
+        return conc_range_slider, conc_optimal_slider
+
+        # if conc_range[0] != conc_range_slider[0]:
+        #     conc_range[0] = None
+        # if conc_range[1] != conc_range_slider[1]:
+        #     conc_range[1] = None
+        # if conc_optimal != conc_optimal_slider:
+        #     conc_optimal = None
+        # return conc_range, conc_optimal
+
+    def read(self, cure_box):
+        """
+        Determines if the cure box is hoovered over or not,
+        and recognizes the information contained within.
+        """
+        info = {}
+        price_box = self._cut_price(cure_box)
+        if price_box is not None:
+            info['price'] = read_integer(price_box)
+            _, name, _ = self._read_cure_name(cure_box)
+            info['name'] = name
+        else:
+            conc_range, conc_optimal = self._read_hoover_cure_concentrations(cure_box)
+            info['conc_range'] = tuple(conc_range)
+            info['conc_optimal'] = conc_optimal
+
+        return info
 
 
 class ProcessBoxReader:
@@ -428,27 +464,19 @@ class ProcessBoxReader:
 
     def _read_concentration(self, process_box):
         conc_box = self._cut_concentration(process_box)
-        im_bw = bio.make_bw(conc_box, th=170)
-        digits = []
-        for digit in range(0, 10, 1):
-            kernel = self.conc_digits[digit]
-            positions = bio.find_shapes(im_bw, kernel, th=160)
-            for pos in positions:
-                x = pos[0]
-                digits.append((x, digit))
-        digits.sort()
+        digits = read_digits_by_kernels(conc_box, self.conc_digits)
         if len(digits) == 2:
-            low = digits[0][1]
-            high = digits[1][1]
+            low = digits[0]
+            high = digits[1]
         elif len(digits) == 3:
-            low = digits[0][1]
-            high = 10*digits[1][1] + digits[2][1]
+            low = digits[0]
+            high = 10*digits[1] + digits[2]
         elif len(digits) == 4:
-            low = 10*digits[0][1] + digits[1][1]
-            high = 10*digits[2][1] + digits[3][1]
+            low = 10*digits[0] + digits[1]
+            high = 10*digits[2] + digits[3]
         else:
-            return [None, None]
-        return [low, high]
+            return None, None
+        return low, high
 
     def read(self, process_box):
         info = {}
