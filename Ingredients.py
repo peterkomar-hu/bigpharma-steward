e38f3f97226cab38b9d16fde27bcb066f55247e4
Peter Komar <peter.komar.hu@gmail.com>
2017-10-14 20:32:49 -0400

    first version of Ingredients class is implemented

--- Ingredients.py
+++ Ingredients.py
@@ -36,7 +36,7 @@ class Effect:
                 return False
         return True
 
-                
+
 class Cure(Effect):
     def __init__(self):
         Effect.__init__(self, effect_type='cure')
@@ -71,3 +71,71 @@ class SideEffect(Effect):
                 if self.remove_conc_range[idx] is None:
                     missing.append('remove_conc_range[{}]'.format(idx))
         return missing
+
+
+class Ingredient:
+    def __init__(self):
+        self.conc = None
+        self.effects = [None, None, None, None]
+
+    def update_from_dict_list(self, dict_list):
+        for idx, effect_dict in enumerate(dict_list):
+            if 'effect_type' not in effect_dict: continue
+            effect_type = effect_dict['effect_type']
+
+            if self.effects[idx] is None:
+                if effect_type == 'empty':
+                    new_effect = Effect(effect_type='empty')
+                elif effect_type == 'cure':
+                    new_effect = Cure()
+                elif effect_type == 'side-effect':
+                    new_effect = SideEffect()
+
+                new_effect.update_from_dict(effect_dict)
+                self.effects[idx] = new_effect
+
+                if self.conc is None and 'conc_current' in effect_dict:
+                    self.conc = effect_dict['conc_current']
+
+            else:
+                self.effects[idx].update_from_dict(effect_dict)
+
+    def missing(self):
+        missing_info_list = []
+        if self.conc is None:
+            missing_info_list.append('conc')
+        for idx, effect in enumerate(self.effects):
+            if effect is None:
+                missing_info_list.append(': '.join(['eff ' + str(idx + 1), 'effect_type']))
+                continue
+            for missing in effect.missing_entries():
+                missing_info_list.append(': '.join(['eff ' + str(idx + 1), missing]))
+        return missing_info_list
+
+    def id_match(self, id_info_dict):
+        mandatory_keys = ['conc_current', 'conc_range', 'effect_type', 'effect_idx']
+        for key in mandatory_keys:
+            assert key in id_info_dict
+        if id_info_dict['conc_current'] != self.conc:
+            return False
+        idx = id_info_dict['effect_idx']
+        stored_effect = self.effects[idx]
+        if id_info_dict['effect_type'] != stored_effect.type:
+            return False
+        if id_info_dict['conc_range'] != stored_effect.conc_range:
+            return False
+        return True
+
+    def is_consistent_with(self, other):
+        if self.conc is not None and \
+                        other.conc is not None and \
+                        self.conc != other.conc:
+            return False
+        for idx in range(4):
+            if self.effects[idx] is None:
+                continue
+            if other.effects[idx] is None:
+                continue
+            if self.effects[idx].is_consistent_with(other.effects[idx]) == False:
+                return False
+        return True
